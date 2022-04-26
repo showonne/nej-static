de91f07246d0033d2221a9fc8850108234080fc3
showonne <showone896@gmail.com>
2022-04-26 23:52:20 +0800

    chore: remove nej

--- nej/src/util/template/demo/b.js
+++
@@ -1,1 +0,0 @@
-console.log('script exline script')
--- nej/src/util/timer/demo/a.js
+++
@@ -1,9 +0,0 @@
-NEJ.define([
-    'util/timer/animation'
-],function(p){
-    var abc = function () {
-        log(+new Date);
-        p.requestAnimationFrame(abc);
-    };
-    p.requestAnimationFrame(abc);
-});
